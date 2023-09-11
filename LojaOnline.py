a1032b41d8332bfdd4e59ba2958d8e7bfdc6798a
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-11 17:57:38 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -72,22 +72,22 @@ if Menu == 'Encomendar':
         mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}'.encode('utf-8')
         send_mail(email, subject, message)
         st.subheader('  Mensagem enviada com Sucesso!') 
-if Menu == 'Pipi das Meias Altas':
+
 
     col1, col2, col3 = st.columns(3)
     
-    with col1:
-        st.header(" Coleção DVD1")
-        st.image("12AF4C15-C421-478E-944C-8F42B9B50185.png")
-        Button1 = st.number_input("Quantidade e adicione ao Carrinho", key="Core")
-    with col2:
-        st.header("DVD 2 PIPI")
-        st.image("07944EEF-7714-493A-94A0-D512BA71DF47.png")
-        Button2 = st.number_input("Quantidade e adicione ao Carrinho", key="Alba")
-    with col3:
-        st.header("A Lassie")
-        st.image("IMG_5123.png")
-        Button3=st.number_input("Quantidade e adicione ao Carrinho", key="Delta")
+with col1:
+    st.header(" Coleção DVD1")
+    st.image("12AF4C15-C421-478E-944C-8F42B9B50185.png")
+    Button1 = st.number_input("Quantidade e adicione ao Carrinho", key="Core")
+with col2:
+    st.header("DVD 2 PIPI")
+    st.image("07944EEF-7714-493A-94A0-D512BA71DF47.png")
+    Button2 = st.number_input("Quantidade e adicione ao Carrinho", key="Alba")
+with col3:
+    st.header("A Lassie")
+    st.image("IMG_5123.png")
+    Button3=st.number_input("Quantidade e adicione ao Carrinho", key="Delta")
 if Menu == 'O Mundo de Patty':
     col1, col2, col3 = st.columns(3)
     with col1:
