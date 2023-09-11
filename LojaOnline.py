e7ab1786e2764500876a7378a4d0076b2cd83549
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-11 17:53:12 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -20,7 +20,7 @@ import smtplib
 import numpy as np
 
 st.title(" A Loja online da Luna -  Livraria e DVDs e Séries")
-Produtos=("Pipi das Meias Altas", " Filmes de Natal", " O Mundo de Patty", " Livros de Aventura", " Livros de Ciencia")
+Produtos=("Pipi das Meias Altas", " Filmes de Natal", " O Mundo de Patty", " Livros de Aventura", " Livros de Ciencia", "Encomendar")
 Menu = st.multiselect("Escolha uma categoria de Produtos", Produtos)
 
 Pesquisa = st.sidebar.text_input("Pesquisa por Livro/DVD")
@@ -60,45 +60,45 @@ with st.sidebar:
     
     Menu = st.multiselect("Escolha uma categoria de Produtos", option_menu)
     
-    if selected == 'Encomendar':
+if Menu == 'Encomendar':
+    
+    email_form = st.form(key='my_email_form', clear_on_submit=False)
+    email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
+    
+    subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
+    message = email_form.text_area (label = ' Escreva a sua Mensagem ')
         
-        email_form = st.form(key='my_email_form', clear_on_submit=False)
-        email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
-        
-        subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
-        message = email_form.text_area (label = ' Escreva a sua Mensagem ')
-            
-        if email_form.form_submit_button(label=' Enviar '):
-            mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}'.encode('utf-8')
-            send_mail(email, subject, message)
-            st.subheader('  Mensagem enviada com Sucesso!') 
+    if email_form.form_submit_button(label=' Enviar '):
+        mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}'.encode('utf-8')
+        send_mail(email, subject, message)
+        st.subheader('  Mensagem enviada com Sucesso!') 
+if Menu == 'Pipi das Meias Altas':
 
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header(" Coleção DVD1")
-    st.image("12AF4C15-C421-478E-944C-8F42B9B50185.png")
-    Button1 = st.number_input("Quantidade e adicione ao Carrinho", key="Core")
-with col2:
-    st.header("DVD 2 PIPI")
-    st.image("07944EEF-7714-493A-94A0-D512BA71DF47.png")
-    Button2 = st.number_input("Quantidade e adicione ao Carrinho", key="Alba")
-with col3:
-    st.header("A Lassie")
-    st.image("IMG_5123.png")
-    Button3=st.number_input("Quantidade e adicione ao Carrinho", key="Delta")
-
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.image("IMG_5131.png")
-    Button4=st.number_input("Quantidade e adicione ao Carrinho", key="Coresey")
-with col2:
-    st.image("IMG_5124.png")
-    Button5=st.number_input("Quantidade e adicione ao Carrinho", key="Ypsilon")
-with col3:
-    st.image("IMG_5130.png")
-    Button6=st.number_input("Quantidade e adicione ao Carrinho", key="image")
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        st.header(" Coleção DVD1")
+        st.image("12AF4C15-C421-478E-944C-8F42B9B50185.png")
+        Button1 = st.number_input("Quantidade e adicione ao Carrinho", key="Core")
+    with col2:
+        st.header("DVD 2 PIPI")
+        st.image("07944EEF-7714-493A-94A0-D512BA71DF47.png")
+        Button2 = st.number_input("Quantidade e adicione ao Carrinho", key="Alba")
+    with col3:
+        st.header("A Lassie")
+        st.image("IMG_5123.png")
+        Button3=st.number_input("Quantidade e adicione ao Carrinho", key="Delta")
+if Menu == 'O Mundo de Patty':
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.image("IMG_5131.png")
+        Button4=st.number_input("Quantidade e adicione ao Carrinho", key="Coresey")
+    with col2:
+        st.image("IMG_5124.png")
+        Button5=st.number_input("Quantidade e adicione ao Carrinho", key="Ypsilon")
+    with col3:
+        st.image("IMG_5130.png")
+        Button6=st.number_input("Quantidade e adicione ao Carrinho", key="image")
 
 col1, col2, col3 = st.columns(3)
 with col1:
