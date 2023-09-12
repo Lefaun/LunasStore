7ea53f2ed64fcc1278ce405f7b38e43e4c790ab6
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-12 13:36:48 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -42,6 +42,20 @@ def send_mail(email, subject, message):
     except Exception as e:
         st.error(f' Ocorreu um Erro ao enviar o e-mail, Desculpe: {e}')
 
+def mail_encomenda():
+        
+    email_form = st.form(key='my_email_form', clear_on_submit=False)
+    email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
+    
+    subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
+    message = email_form.text_area (label = ' Escreva a sua Mensagem ')
+    encomenda = email_form.text_area (label = ' Artigos e Quantidade ' , value = Encomendas)
+    
+    if email_form.form_submit_button(label=' Enviar '):
+        mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
+        send_mail(email, subject, message,  )
+        st.subheader('  Mensagem enviada com Sucesso!')
+
 with st.sidebar:
 
     
@@ -151,6 +165,9 @@ if choice == " Filmes de Natal":
     with col1:
         st.header(Encomendas)
     with col2: 
-        st.markdown('<a href="Encomendar" target=_Self>Encomendar</a>', unsafe_allow_html=True)
+        Confirmar = st.Button("Confirmar")
+    if Confirmar == True:
+        mail_encomenda()
+        
         
 
