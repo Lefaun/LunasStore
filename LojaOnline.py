1a5e2e2028046caee2f8781dd3aee6a72b226d6c
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-12 11:12:41 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -102,19 +102,19 @@ if choice == 'Pipi das Meias Altas':
         st.header(Encomendas)
     with col2:
         Confirmar = st.button("Confirmar")
-    if Confirmar == True:
-        email_form = st.form(key='my_email_form2', clear_on_submit=False)
-        email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
-        
-        subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
-        message = email_form.text_area (label = ' Escreva a sua Mensagem ')
-        encomenda = email_form.text_area (label = ' Artigos e Quantidade ', value = Encomendas )
+if Confirmar == True:
+    email_form = st.form(key='my_email_form2', clear_on_submit=False)
+    email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
+    
+    subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
+    message = email_form.text_area (label = ' Escreva a sua Mensagem ')
+    encomenda = email_form.text_area (label = ' Artigos e Quantidade ', value = Encomendas )
 
-    if st.form_submit_button(label='Enviar', key="Psy"):
-        mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
-        send_mail(email, subject, message, )
-        st.subheader('  Mensagem enviada com Sucesso!') 
-        st.write(Encomendas)
+if st.form_submit_button(label='Enviar', key="Psy"):
+    mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
+    send_mail(email, subject, message, )
+    st.subheader('  Mensagem enviada com Sucesso!') 
+    st.write(Encomendas)
 
 if choice == ' O Mundo de Patty':
     col1, col2, col3 = st.columns(3)
