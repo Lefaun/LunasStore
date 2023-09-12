16f7b94beb736c0ee292a352ec9adee5a0120901
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-12 11:03:22 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -101,7 +101,7 @@ if choice == 'Pipi das Meias Altas':
     with col1:
         st.header(Encomendas)
     with col2:
-        Confirmar = st.submit_button("Confirmar")
+        Confirmar = st.button("Confirmar")
     if Confirmar == True:
         email_form = st.form(key='my_email_form2', clear_on_submit=False)
         email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
