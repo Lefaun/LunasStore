5e7d8bb142f7bad46554a5805e03ca8945d52eb1
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-12 14:01:22 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -44,7 +44,7 @@ def send_mail(email, subject, message):
 
 def mail_encomenda():
         
-    email_form = st.form()
+    email_form = st.form(key='my_email_form9', clear_on_submit=False)
     email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
     
     subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
