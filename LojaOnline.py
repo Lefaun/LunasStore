7afd5537cddbf4ff80c709cf3006880011a96296
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-12 14:54:40 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -175,7 +175,7 @@ if choice == " Filmes de Natal":
         encomenda = email_form.text_area (label = ' Artigos e Quantidade ' , value = Encomendas)
         
     
-    if email_form.form_submit_button(label=' Submeter Pedido ' , key="Pseudo"):
+    if email_form.form_submit_button(label=' Submeter Pedido '):
         mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
         send_mail(email, subject, message)
         st.subheader('  Mensagem enviada com Sucesso!')
