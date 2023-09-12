946dc60c34678fb75fe9a5109a7b9e502acf96f3
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-12 14:24:36 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -73,19 +73,19 @@ with st.sidebar:
     
     
 if choice == 'Encomendar':
-    def mailcom(email_form, subject, email,message,messagem, encomenda):
-        email_form = st.form(key='my_email_form2', clear_on_submit=False)
-        email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
-        
-        subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
-        message = email_form.text_area (label = ' Escreva a sua Mensagem ')
-        encomenda = email_form.text_area (label = ' Artigos e Quantidade ', value = Encomendas )
-        
-        if email_form.form_submit_button(label=' Enviar '):
-            mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
-            send_mail(email, subject, message, )
-            st.subheader('  Mensagem enviada com Sucesso!') 
-            st.write(Encomendas)
+    
+    email_form = st.form(key='my_email_form2', clear_on_submit=False)
+    email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
+    
+    subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
+    message = email_form.text_area (label = ' Escreva a sua Mensagem ')
+    encomenda = email_form.text_area (label = ' Artigos e Quantidade ', value = Encomendas )
+    
+    if email_form.form_submit_button(label=' Enviar '):
+        mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
+        send_mail(email, subject, message, )
+        st.subheader('  Mensagem enviada com Sucesso!') 
+        st.write(Encomendas)
 if choice == 'Pipi das Meias Altas':
     col1, col2, col3 = st.columns(3)
         
@@ -167,7 +167,8 @@ if choice == " Filmes de Natal":
         Confirmar = st.button("Confirmar")
 
     if Confirmar == True:
-        mailcom(email_form, subject, email,message,messagem, encomenda)
+        mail_encomenda(email_form, subject, email,message,messagem, encomenda)
+        send_mail()
         
         
 
