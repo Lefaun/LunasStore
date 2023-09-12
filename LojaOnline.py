385b27dc1c58ef393bd8276a8e8ecf4ce2829e22
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-12 14:21:09 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -73,7 +73,7 @@ with st.sidebar:
     
     
 if choice == 'Encomendar':
-    def mailcom(email_form, subject, email,message,messagem, encomenda) 
+    def mailcom(email_form, subject, email,message,messagem, encomenda):
         email_form = st.form(key='my_email_form2', clear_on_submit=False)
         email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
         
