b4f1674f89de179184126d6c6c6c8a1acce6f051
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-12 16:22:27 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -176,11 +176,11 @@ if choice == " Filmes de Natal":
         message = email_form.text_area (label = ' Escreva a sua Mensagem ')
         encomenda = email_form.text_area (label = ' Artigos e Quantidade ', value = Encomendas )
     
-    if email_form.form_submit_button(label='Enviar'):
-        mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
-        send_mail(email, subject, message, )
-        st.subheader('  Mensagem enviada com Sucesso!') 
-        st.write(Encomendas)
+        if email_form.form_submit_button(label='Enviar'):
+            mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
+            send_mail(email, subject, message, )
+            st.subheader('  Mensagem enviada com Sucesso!') 
+            st.write(Encomendas)
         
         #email_form = st.form(key='my_email_form5', clear_on_submit=False)
         #email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
