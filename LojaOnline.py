f288dfdc413af69c8e6a74fd2fe4ae69263e5634
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-13 14:44:37 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -49,18 +49,18 @@ class EmailSend():
         
     def mail(email_form, email, subject, message, encomenda):
             
-        email_form = st.form(key='my_email_form8', clear_on_submit=False)
+        email_form = st.form(key='my_email_form4', clear_on_submit=False)
         email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
-        
+    
         subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
         message = email_form.text_area (label = ' Escreva a sua Mensagem ')
-        encomenda = email_form.text_area (label = ' Artigos e Quantidade ' , value = Encomendas)
-        
-        if email_form.form_submit_button(label=' Enviar '):
+        encomenda = email_form.text_area (label = ' Artigos e Quantidade ', value = Encomendas )
+    
+        if email_form.form_submit_button(label='Enviar'):
             mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
-            send_mail(email, subject, message,  )
-            st.subheader('  Mensagem enviada com Sucesso!')
-
+            send_mail(email, subject, message, )
+            st.subheader('  Mensagem enviada com Sucesso!') 
+            st.write(Encomendas)
 with st.sidebar:
 
        
