f66c3531c876c87d7f203039f5a59fe05f13ccc0
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-13 16:23:04 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -141,9 +141,19 @@ if choice == 'Pipi das Meias Altas':
 
     if Confirmar == True:
         
-       EmailSend.mail(email_form, email, subject, message, encomenda,)
-       EmailSend.send_mail(email, subject, message,)
+        email_form = st.form(key='my_email_form2', clear_on_submit=False)
+        email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
         
+        subject = email_form.text_input (label = ' Escreva aqui o Assunto ' )
+        message = email_form.text_area (label = ' Escreva a sua Mensagem ')
+        encomenda = email_form.text_area (label = ' Artigos e Quantidade ', value = Encomendas )
+
+        if email_form.form_submit_button(label='Enviar'):
+            mensagem = f'Subject:{subject}\n\n De: {email}\n\n Assunto: {message}, Artigos: {encomenda}'.encode('utf-8')
+            send_mail(email, subject, message, )
+            st.subheader('  Mensagem enviada com Sucesso!') 
+            st.write(Encomendas)
+
 if choice == ' O Mundo de Patty':
     col1, col2, col3 = st.columns(3)
     with col1:
