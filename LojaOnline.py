c82800cc0e0291726bd940c43c1d0ff19087d0d3
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-14 22:52:16 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -80,7 +80,7 @@ class EmailSend():
 
 with st.sidebar:
 
-    st.image("IMG_2171.jpg" , width=300)  
+    st.image("AI_Monster_Book_eater.png" , width=300)  
     email_form = st.form(key='my_email_form', clear_on_submit=False)
     email = email_form.text_input(label='Por Favor Escreva o Seu Endereço de e-mail')
     
