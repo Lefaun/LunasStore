84d50c7df9628bd507df9fdcd2fac2e2e16493af
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-19 18:56:10 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -84,18 +84,7 @@ with st.sidebar:
     url = (f"https://docs.google.com/spreadsheets/d/{search}/gviz/tq?tqx=out:csv&sheet={search}")
     df = pd.read_csv(url, dtype=str)
     
-    # Page setup
-    st.set_page_config(page_title="Search Engine", layout="wide")
-    st.title("Pesquise aqui o Seus Livros")
     
-    # Connect to the Google Sheet
-    sheet_id = ("https://docs.google.com/spreadsheets/d/1cRZcbJAjLzwoLzA4K1R_ldnjXMU1obLDKjWCR5prsAU/edit?usp=sharing")
-    sheet_name = ("search")
-    url = (f"<https://docs.google.com/spreadsheets/d/{search}/gviz/tq?tqx=out:csv&sheet={search}>")
-    df = pd.read_csv(url, dtype=str).fillna("")
-
-
-    # Show the dataframe (we'll delete this later)
 
     
     email_form = st.form(key='my_email_form', clear_on_submit=False)
