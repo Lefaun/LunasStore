fc8009d1cb03617b75ad621b77b290ae6340312f
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-19 18:53:48 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -81,17 +81,17 @@ class EmailSend():
 with st.sidebar:
 
     st.image("AI_Monster_Book_eater.png" , width=300)
-    url = f"https://docs.google.com/spreadsheets/d/{search}/gviz/tq?tqx=out:csv&sheet={search}"
+    url = (f"https://docs.google.com/spreadsheets/d/{search}/gviz/tq?tqx=out:csv&sheet={search}")
     df = pd.read_csv(url, dtype=str)
     
     # Page setup
-    st.set_page_config(page_title="Python Talks Search Engine", page_icon="🐍", layout="wide")
-    st.title("Python Talks Search Engine")
+    st.set_page_config(page_title="Search Engine". layout="wide")
+    st.title("Pesquise aqui o Seus Livros")
     
     # Connect to the Google Sheet
-    sheet_id = "https://docs.google.com/spreadsheets/d/1cRZcbJAjLzwoLzA4K1R_ldnjXMU1obLDKjWCR5prsAU/edit?usp=sharing"
+    sheet_id = ("https://docs.google.com/spreadsheets/d/1cRZcbJAjLzwoLzA4K1R_ldnjXMU1obLDKjWCR5prsAU/edit?usp=sharing")
     sheet_name = "search"
-    url = f"<https://docs.google.com/spreadsheets/d/{search}/gviz/tq?tqx=out:csv&sheet={search}>"
+    url = (f"<https://docs.google.com/spreadsheets/d/{search}/gviz/tq?tqx=out:csv&sheet={search}>")
     df = pd.read_csv(url, dtype=str).fillna("")
 
 
