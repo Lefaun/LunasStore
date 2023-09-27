cc90f9d8de3bf9496de071b7ad56e635d678fdc2
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-27 10:28:10 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -79,7 +79,7 @@ class ListarNome():
             Livros_a_Verificar = ['O Mundo de Patty', 'Filme de Natal']
             
             # check if the 'fruit' column contains any of the fruits we are interested in
-            filter = [livro in Livros_a_Verificar for livro1 in df['Livros']]
+            filter = [livro1 in Livros_a_Verificar for livro in df['Livros']]
             
             # print the resulting DataFrame, containing only the rows that match the mask
             
