86debcf800e8aa52cbcd5942a7cfb94484141098
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-27 08:28:19 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -65,7 +65,7 @@ class ListarNome():
             
     def Consultar(self):
         livro = st.text_input("Consulte um Livro")
-        if livro in df:
+        if livro in reader:
             st.write(f"este {livro} encontra-se na Lista")
         else:
             st.write(f"este {livro} não encontra-se na Lista")
