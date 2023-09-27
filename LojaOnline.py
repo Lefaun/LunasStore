dba90d79a010de26c4a74b262b32a385cf2285a6
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-27 09:57:30 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -72,7 +72,7 @@ class ListarNome():
         with open('Lista_de_Livros - Folha1.csv', 'r') as file:
             reader = csv.reader(file)
             df = pd.DataFrame(reader)
-            Search = df['Livros'].isin(livro)
+            Search = df['0'].isin(livro)
             #for row in reader:
             if  livro in Search:
                 st.write(f"este {livro} encontra-se na Lista")
