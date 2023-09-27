1f995337934edbacf73496c738b80ab71f3fa99e
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-27 14:42:21 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -192,7 +192,7 @@ Encomendas = []
 Pesquisa = st.sidebar.text_input("Pesquisa por Livro/DVD")
 button = st.sidebar.button("Pesquise Por TItulo", )
 if button == True:
-    option == "Encomendar":
+    if option == "Encomendar":
         Menu_Completo()
 
 def send_mail(email, subject, message):
