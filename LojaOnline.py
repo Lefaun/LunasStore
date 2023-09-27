fc222a4d0dbab8836ad01ab76a196eb7570a070f
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-09-27 09:17:43 +0100

    Update LojaOnline.py

--- LojaOnline.py
+++ LojaOnline.py
@@ -71,11 +71,11 @@ class ListarNome():
         livro = st.text_input("Consulte um Livro")
         with open('Lista_de_Livros - Folha1.csv', 'r') as file:
             reader = csv.reader(file)
-            for row in reader:
-                if  livro in reader:
-                    st.write(f"este {livro} encontra-se na Lista")
-                else:
-                    st.write(f"este {livro} não encontra-se na Lista")
+            if  livro == reader:
+                st.write(f"este {livro} encontra-se na Lista")
+            else:
+                st.write(f"este {livro} não encontra-se na Lista")
+   
     def Menu_Completo(self):
         
         st.write("Escolha uma opção:")
